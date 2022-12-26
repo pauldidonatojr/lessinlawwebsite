d3330452e480e1ffc2ce9c59b17efef6f1622e93
PJ <87795990+pauldidonatojr@users.noreply.github.com>
2022-12-26 17:58:46 -0500

    push me

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,84 +3,79 @@ import styled from 'styled-components'
 import { useForm, ValidationError } from '@formspree/react'
 
 const Contact = () => {
- const [state, handleSubmit] = useForm('xzbwkndk')
+ const [state, handleSubmit] = useForm('xpznvabl')
  if (state.succeeded) {
-  return <p style={{ fontSize: '2rem' }}>We will be in contact shortly!</p>
+    return (
+       <div style={{
+          height: '100vh', width: "50vh",
+          backgroundColor: 'lightblue',
+          display: 'grid',
+          justifyContent: 'center',
+          alignItems: 'center'
+
+       }}>
+       <p style={{ fontSize: '1.6rem' }}>We will be in contact shortly!</p>
+  </div>
+    )
  }
  return (
   <Wrapper>
-   <div class="form">
+   <form onSubmit={handleSubmit}>
     <div class="subtitle">Fill the form we will contact you</div>
 
     <div className="overstop">
      <div class="input-container ic1">
-      <input id="firstname" class="input" type="text" placeholder=" " />
+      <input id="firstname" type="firstname" name="firstname" class="input" />
+
       <div class="cut"></div>
-      <label for="firstname" class="placeholder">
+
+      <label htmlFor="firstname" class="placeholder">
        Name
       </label>
+
+      <ValidationError
+       prefix="Firstname"
+       field="firstname"
+       errors={state.errors}
+      />
      </div>
      <div class="input-container ic2">
-      <input id="phone" class="input" type="text" placeholder=" " />
+      <input id="number" type="number" name="number" class="input" />
       <div class="cut"></div>
-      <label for="phone" class="placeholder">
-       Phone
+
+      <label htmlFor="number" class="placeholder">
+       Phone Number
       </label>
+      <ValidationError prefix="Phone" field="phone" errors={state.errors} />
      </div>
+
      <div class="input-container ic2">
       <input id="email" class="input" type="text" placeholder=" " />
       <div class="cut cut-short"></div>
-      <label for="email" class="placeholder">
+      <label htmlFor="email" class="placeholder">
        Email
       </label>
+      <ValidationError prefix="Email" field="email" errors={state.errors} />
      </div>
 
      <div class="input-container2">
       <div class="cut cut-short"></div>
 
       <textarea
+       id="message"
+       name="message"
        className="input"
-       name="description"
        rows="5"
        cols="50"
        style={{ height: '12rem' }}
       ></textarea>
-      {/* <label for="message" class="placeholder">
-      Message
-     </label> */}
      </div>
 
-     <button type="text" class="submit">
+     <button type="submit" class="submit" disabled={state.submitting}>
       submit
      </button>
     </div>
-   </div>
-
-   {/* <form
-      className="contact-form"
-      onSubmit={handleSubmit}
-      style={{ color: 'black' }}
-     >
-      <label htmlFor="email">Email Address</label>
-      <input id="email" type="email" name="email" className="form-input" />
-      <ValidationError prefix="Email" field="email" errors={state.errors} />
-
-      <label htmlFor="name">Name</label>
-      <input id="name" type="name" name="name" className="form-input" />
-
-      <label htmlFor="number">Phone Number</label>
-      <input id="number" type="number" name="number" className="form-input" />
-
-      <label htmlFor="message">Message</label>
-      <textarea id="message" type="message" name="message" />
-
-      <button type="submit" className="btn" disabled={state.submitting}>
-       Submit
-      </button>
-     </form> */}
-   {/* <p className="info" style={{ color: 'black' }}>
-      Fill out the form and we will be in contact with you shortly!
-     </p> */}
+   </form>
   </Wrapper>
  )
 }
@@ -102,7 +97,16 @@ const Wrapper = styled.section`
   width: 100%;
   margin-bottom: 5.5rem;
  }
-
+ form {
+  background-color: #15172b;
+  border-radius: 20px;
+  box-sizing: border-box;
+  height: auto;
+  padding: 20px;
+  height: 850px;
+  width: 100%;
+  margin-bottom: 5.5rem;
+ }
  .title {
   color: #eee;
   font-family: sans-serif;
