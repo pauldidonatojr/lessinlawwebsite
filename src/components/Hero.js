890572f2867f531d0e5ffdf9ff0641d7671cc1d0
Dawar Mustaqeem <45918546+dmustaqeem@users.noreply.github.com>
2023-01-16 19:29:56 +0500

    updated Hero ContactUS Button

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,15 +9,17 @@ import Card from '@mui/joy/Card';
 import CardCover from '@mui/joy/CardCover';
 import CardContent from '@mui/joy/CardContent';
 import Typography from '@mui/joy/Typography';
+import Backdrop from '@mui/material/Backdrop'
 import CallIcon from '@mui/icons-material/Call';
 import EmailIcon from '@mui/icons-material/Email';
+import CloseIcon from '@mui/icons-material/Close';
 
 
 function CarouselItems(props) {
     return (
         <CardWrapper>
             <div className='cardHolder'>
-                <Card sx={{ minHeight: '280px', width: '100%'}}>
+                <Card sx={{ minHeight: '280px', width: '100%' }}>
                     <CardCover>
                         <img
                             src={props.item.source}
@@ -45,6 +47,15 @@ function CarouselItems(props) {
 }
 
 const Hero = () => {
+
+    const [open, setOpen] = React.useState(false)
+    const handleClose = () => {
+      setOpen(false)
+    }
+    const handleToggle = () => {
+      setOpen(!open)
+    }
+
     const Item = styled(Paper)(({ theme }) => ({
         backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
         ...theme.typography.body2,
@@ -85,19 +96,67 @@ const Hero = () => {
         alert('CONTACT US CLICKED');
     }
 
+    const card = (
+        <ContactCard>
+        <div className='card'>
+        <div class="background">
+          <div class="container">
+            <div class="screen">
+              <div class="screen-body">
+                <div class="screen-body-item left">
+                <div class="screen-header-buttonClose"><CloseIcon style={{cursor: 'pointer'}} onClick={handleClose}></CloseIcon></div>
+                  <div class="app-contact">CONTACT INFO : +62 81 314 928 595</div>
+                </div>
+                <div class="screen-body-item">
+                  <div class="app-form">
+                    <div class="app-form-group">
+                      <input class="app-form-control" placeholder="NAME"/>
+                    </div>
+                    <div class="app-form-group">
+                      <input class="app-form-control" placeholder="EMAIL"/>
+                    </div>
+                    <div class="app-form-group">
+                      <input class="app-form-control" placeholder="CONTACT NO"/>
+                    </div>
+                    <div class="app-form-group message">
+                      <input class="app-form-control" placeholder="MESSAGE"/>
+                    </div>
+                    <div class="app-form-group buttons">
+                      <button class="app-form-button">SEND</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+        </ContactCard>
+    )
+
     return (
         <Wrapper>
+            
+            <Backdrop
+                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={open}
+                
+            >
+                <Card>{card}</Card>
+            </Backdrop>
+
             <Header />
             <div>
                 <div className='backg'>
-                <img className='dekstopImage' src='/android-chrome-512x512.png'></img>
+                    <img className='dekstopImage' src='/android-chrome-512x512.png'></img>
                     <div className='detailsTextHolder'>
                         <div className='detailsText'>
                             Our attorneys are dedicated to protecting the civil rights of our clients. We do this by aggressively pursuing compensation for them in court, seeking payment for the damage caused by police and other public employees.
                         </div>
                         <div className='contactButtonHolder'>
-                            <div className='contactButton'>Contact US</div>
-                            </div>
+                            <div onClick={handleToggle} className='contactButton'>Contact US</div>
+                        </div>
                     </div>
                 </div>
 
@@ -110,16 +169,13 @@ const Hero = () => {
                         </div>
 
                         <div className='carousel'>
-                        <Carousel>
-                            {
-                                items.map((item, i) => <CarouselItems key={i} item={item} />)
-                            }
-                        </Carousel>
-                        
+                            <Carousel>
+                                {
+                                    items.map((item, i) => <CarouselItems key={i} item={item} />)
+                                }
+                            </Carousel>
+
                         </div>
-
-
-
                     </div>
                 </div>
             </div>
@@ -129,6 +185,239 @@ const Hero = () => {
     )
 
 }
+
+const ContactCard = styles.div`
+height: 100%;
+width: 100%;
+.background {
+    display: flex;
+    min-height: 100vh;
+  }
+  
+  .container {
+    flex: 0 1 700px;
+    margin: auto;
+    padding: 10px;
+  }
+  
+  .screen {
+    position: relative;
+    background: #3e3e3e;
+    border-radius: 15px;
+  }
+  
+  .screen:after {
+    content: '';
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 20px;
+    right: 20px;
+    bottom: 0;
+    border-radius: 15px;
+    box-shadow: 0 20px 40px rgba(0, 0, 0, .4);
+    z-index: -1;
+  }
+  
+  .screen-header {
+    display: flex;
+    align-items: center;
+    padding: 10px 20px;
+    background: #4d4d4f;
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+  }
+  
+  .screen-header-left {
+    margin-right: auto;
+  }
+  
+  .screen-header-button {
+    display: inline-block;
+    width: 8px;
+    height: 8px;
+    margin-right: 3px;
+    border-radius: 8px;
+    background: white;
+  }
+
+  .screen-header-buttonClose {
+    display: inline-block;
+    width: 8px;
+    height: 8px;
+    margin-right: 3px;
+    border-radius: 8px;
+  }
+  
+  .screen-header-button.close {
+  }
+  
+  .screen-header-button.maximize {
+    background: #e8e925;
+  }
+  
+  .screen-header-button.minimize {
+    background: #74c54f;
+  }
+  
+  .screen-header-right {
+    display: flex;
+  }
+  
+  .screen-header-ellipsis {
+    width: 3px;
+    height: 3px;
+    margin-left: 2px;
+    border-radius: 8px;
+    background: #999;
+  }
+  
+  .screen-body {
+    display: flex;
+  }
+  
+  .screen-body-item {
+    flex: 1;
+    padding: 50px;
+  }
+  
+  .screen-body-item.left {
+    display: flex;
+    flex-direction: column;
+  }
+  
+  .app-title {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    color: #ea1d6f;
+    font-size: 26px;
+  }
+  
+  .app-title:after {
+    content: '';
+    display: block;
+    position: absolute;
+    left: 0;
+    bottom: -10px;
+    width: 25px;
+    height: 4px;
+    background: #ea1d6f;
+  }
+  
+  .app-contact {
+    margin-top: auto;
+    font-size: 8px;
+    color: #888;
+  }
+  
+  .app-form-group {
+    margin-bottom: 15px;
+  }
+  
+  .app-form-group.message {
+    margin-top: 40px;
+  }
+  
+  .app-form-group.buttons {
+    margin-bottom: 0;
+    text-align: right;
+  }
+  
+  .app-form-control {
+    width: 100%;
+    padding: 10px 0;
+    background: none;
+    border: none;
+    border-bottom: 1px solid #666;
+    color: #ddd;
+    font-size: 14px;
+    text-transform: uppercase;
+    outline: none;
+    transition: border-color .2s;
+  }
+  
+  .app-form-control::placeholder {
+    color: #666;
+  }
+  
+  .app-form-control:focus {
+    border-bottom-color: #ddd;
+  }
+  
+  .app-form-button {
+    background: none;
+    border: none;
+    color: white;
+    font-size: 14px;
+    cursor: pointer;
+    outline: none;
+  }
+  
+  .app-form-button:hover {
+    color: brown;
+    font-weight: bold;
+  }
+  
+  .credits {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+    color: #ffa4bd;
+    font-family: 'Roboto Condensed', sans-serif;
+    font-size: 16px;
+    font-weight: normal;
+  }
+  
+  .credits-link {
+    display: flex;
+    align-items: center;
+    color: #fff;
+    font-weight: bold;
+    text-decoration: none;
+  }
+  
+  .dribbble {
+    width: 20px;
+    height: 20px;
+    margin: 0 5px;
+  }
+  
+  @media screen and (max-width: 520px) {
+    .screen-body {
+      flex-direction: column;
+    }
+  
+    .screen-body-item.left {
+      margin-bottom: 30px;
+    }
+  
+    .app-title {
+      flex-direction: row;
+    }
+  
+    .app-title span {
+      margin-right: 12px;
+    }
+  
+    .app-title:after {
+      display: none;
+    }
+  }
+  
+    @media only screen and (max-width: 600px) {
+     
+  .screen-body {
+      padding: 40px;
+    }
+  
+    .screen-body-item {
+      padding: 0;
+    }
+    }
+
+`
 
 const CardWrapper = styles.div`
 height: 100%;
