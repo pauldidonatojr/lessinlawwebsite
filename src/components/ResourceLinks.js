8b3d8f96b41e96999d9f519ffe5bf1be77966577
Dawar Mustaqeem <45918546+dmustaqeem@users.noreply.github.com>
2023-01-23 20:06:44 +0500

    updated resource links

--- src/components/ResourceLinks.js
+++ src/components/ResourceLinks.js
@@ -1,10 +1,10 @@
-import styleM from 'styled-components'
+import styled from 'styled-components'
 import React, { useState, useEffect } from 'react'
 import { FaWindowClose } from 'react-icons/fa'
 import { useModalContext } from '../context/modal_context'
 import Footer from './Footer'
 import Header from './Header'
-import { styled } from '@mui/material/styles';
+//import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
@@ -19,25 +19,134 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 const ResourceLinks = () => {
-  const { openModal } = useModalContext()
-
-  const { isModalOpen, closeModal } = useModalContext()
   return (
-    <Wrapper>
+    <div>
       <Header />
-      <div className='image-holder'>
-        <img src='./links.jpg' style={{ position: 'relative', width: '100%', height: '50vh', top: '0px', left: '0px', objectFit: 'cover' }}></img>
-        <div
-          className='info-holder'
-        >
+      <Wrapper>
 
-          <div className='article-holder'>
-            <h2 style={{ opacity: '1' }}> Resource Links </h2>
-          </div>
+        <div className='backimage'>
+          <img src='/links.jpg' style={{ height: '100%', width: '100%', objectFit: 'cover' }}></img>
         </div>
-      </div>
+        <div className='transparentBackground'></div>
+        <div className='textHolder'>
+          <div className='header'>
+            Practice Areas
+          </div>
+          <div className='innerData'>Our attorneys are dedicated to helping people obtain compensation after experiencing violations of their constitutionally protected rights. Many of these cases are complex, involving multiple areas of constitutional law. At the law firm of Jeffrey R. Lessin & Associates, P.C., we have both the knowledge and the determination to pursue your case as far as necessary to obtain justice.</div>
+        </div>
 
-      <Box className = 'box-container'>
+
+        <div className='cardsHolder'>
+
+          <div className="sectionCenter">
+            <article className="product-card">
+              <div className="overlay-product" />
+              <div className="product-details">
+                <div className='product-header'>Hey</div>
+                <div className='links'>
+                  <a className='link' href='http://maps.google.com/'>Google Maps</a>
+
+                  <a className='link' href='http://www.nws.noaa.gov/'>National Weather Service</a>
+
+                  <a className='link' href='http://maps.yahoo.com/'>Yahoo! Maps</a>
+
+                  <a className='link' href='http://www.yellowpages.com/'>YelloPages.com</a>
+                </div>
+              </div>
+            </article>
+          </div>
+
+          <div className="sectionCenter">
+            <article className="product-card">
+              <div className="overlay-product" />
+              <div className="product-details">
+                <header className='product-header'>
+                  <h4 style={{ color: 'white' }}>Prison Guard Misconduct</h4>
+                </header>
+                <div className='links'>
+                  <a className='link' href='http://www.nytimes.com/'>New York Times</a>
+
+                  <a className='link' href='http://www.newspapers.com/'>Newspapers<br></br> Online</a>
+
+                  <a className='link' href='http://www.usatoday.com/'>USA Today</a>
+
+                  <a className='link' href='http://www.wsj.com/'>Wall Street Journal</a>
+
+                </div>
+              </div>
+            </article>
+          </div>
+
+          <div className="sectionCenter">
+            <article className="product-card">
+              <div className="overlay-product" />
+              <div className="product-details">
+                <header className='product-header'>
+                  <h4 style={{ color: 'white' }}>Institutional Assault</h4>
+                </header>
+                <div className='links'>
+                  <a className='link' href='http://www.uscourts.gov/'>US Court</a>
+
+                  <a className='link' href='http://west.thomson.com/'>West</a>
+
+                  <a className='link' href='http://www.westlaw.com/'> West Law</a>
+
+                </div>
+              </div>
+            </article>
+          </div>
+
+        </div>
+
+
+        <div className='cardsHolder'>
+
+          <div className="sectionCenter">
+            <article className="product-card">
+              <div className="overlay-product" />
+              <div className="product-details">
+                <div className='product-header'>
+                  <h4 style={{ color: 'white' }}>Racial Profiling</h4>
+                </div>
+                <div className='links'>
+                  <a  className='link' href = 'http://www.google.com/'>Google</a>
+                  
+                  <a  className='link' href = 'http://search.msn.com/'>MSN</a>
+                  
+                  <a  className='link' href = 'http://search.yahoo.com/'>Yahoo!</a>
+                
+              </div>
+              </div>
+            </article>
+          </div>
+
+          <div className="sectionCenter">
+            <article className="product-card">
+              <div className="overlay-product" />
+              <div className="product-details">
+                <header className='product-header'>
+                  <h4 style={{ color: 'white' }}>Prison Guard Misconduct</h4>
+                </header>
+                <div className='links'>
+                  <a  className='link' href = 'https://www.usa.gov/'>FirstGov</a>
+                  
+                  <a  className='link' href = 'http://www.loc.gov/'>Library of Congress</a>
+                  
+                  <a  className='link' href = 'http://www.house.gov/'>U.S. House of Representatives</a>
+              
+                  <a  className='link' href = 'http://www.senate.gov/'>U.S. Senate</a>
+                  
+                  <a  className='link' href = 'http://www.whitehouse.gov/'>White House</a>
+              
+              </div>
+              </div>
+            </article>
+          </div>
+
+
+        </div>
+
+        {/* <Box className = 'box-container'>
         <Grid style ={{marginLeft :'15%',marginTop : '2%'}} container spacing={2}>
         <Grid style={{borderRadius:'25px'}} item xs={4}>
             <Item className='grid-item1'><div className='titlebar'>
@@ -134,28 +243,76 @@ const ResourceLinks = () => {
               </Item>
           </Grid>
           </Grid> 
-      </Box>
+      </Box> */}
 
+
+      </Wrapper>
       <Footer />
-    </Wrapper>
+    </div>
   )
 }
 
 
 export default ResourceLinks
-const Wrapper = styleM.section`
-height: 100vh;
+const Wrapper = styled.section`
+height: 100%;
 width: 100%;
  
+.backimage{
+  position: absolute;
+  z-index : -1;
+  height: 75vh;
+  width: 100%;
+}
 
-.image-holder{
-   display: inline;
-   justify-content: center;
-   align-items: center; 
-   width: 100%;
-   height: auto;
-   position: relative;
+/* New */
+
+.transparentBackground{
+  width: 100%;
+  height: 75vh;
+  background-color: black;
+  opacity: 0.5;
+  position: absolute;
+}
+
+.dataHolder{
+  height: 75vh;
+  width: 100%;
+  position: relative;
+  display: grid;
+  place-content: center; 
+}
+
+.textHolder{
+  width: 100%;
+  height: 75vh;
+  display: grid;
+  place-content: center;
+}
+
+ .header{
+  position: relative;
+  display: grid;
+  place-content: center;
+  font-size: 30px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  color: white;
  }
+
+ .innerData{
+  position: relative;
+  display: grid;
+  place-content: center;
+  text-align:center;
+  font-size: 15px;
+  font-family: sans;
+  letter-spacing: 1px;
+  color: white;
+  font-style: italic;
+  padding: 20px;
+ }
+
  .image-news{
   padding : 1%;
   width : 100%;
@@ -207,12 +364,8 @@ width: 100%;
 }
 .box-container{
   flexGrow: 2;
-  background-color ;
-  padding: 1%;
-  width: 80%;
-  border-radius : 30px;
-  margin-left: 10%;
-
+  width: 100%;
+  border-radius : 20px;
 }
 .image-card{
 
@@ -223,25 +376,17 @@ width: 100%;
 
 }
 .link {
-
+  width: 100%;
   text-decoration: none;
-  padding:5%;
   border-radius: 10px;
-  margin: 1%;
-  font-family : sans;
-  font-style: italic;
   color : white;
-  font-size:25px;
-  box-shadow: 1px 1px 2px(blue);
-}
-.links{
-
-  padding : 10%;
-  text-color : black;
+  font-size: 15px;
+  display: grid;
+  place-content: center;
+  text-align: center;
 }
 
 .link:hover{
-   
   box-shadow: 10px 10px 5px rgba(0, 0, 0, 0.2);
   -webkit-transform: scale(1.1);
   transform: scale(1.1);
@@ -269,21 +414,18 @@ width: 100%;
   }
 
  .links{
-
-  
-    font-size : 30px;
     display : grid;
-    padding : 2%;
-
-
+    padding : 10px;
+    width: 100%;
+    height: 90%;
  }
  
  .article-holder{
      height: 100%;
      width: 100%;
      color: white;
-     borderBottom: 2px solid #dadada;
-     borderColor: 'white';
+     border-bottom: 2px solid #dadada;
+     border-color: 'white';
      opacity: 1;
      
  }
@@ -422,8 +564,123 @@ width: 100%;
    background-color: #06b;
  }
 
- @media only screen and (max-width: 600px) {
+
+
+
+
  
+ .sectionCenter {
+  display: grid;
+  justify-items: center;
+  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+  background-color: var(--blue);
+  color: cornsilk;
+}
+
+.product-card {
+  display: grid;
+  height: 70vh;
+  width: 50vh;
+  margin-top: 4rem;
+  margin-bottom: 1rem;
+  /* transition: all 0.7s cubic-bezier(0.895, 0.03, 0.685, 0.22); */
+  transition: box-shadow 0.3s;
+  background-color: rgb(20, 39, 79);
+  &:hover{
+    box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.04), 0px 2px 6px rgba(9, 55, 53, 0.08),
+    0px 16px 24px rgba(9, 55, 53, 0.1), 0px 24px 32px rgba(9, 55, 53, 0.14); 
+  }
+}
+
+.card-image{
+  object-fit:cover;
+  height: 70vh;
+  width: 50vh;
+  border-radius: var(--radius);
+  display: block;
+  position: absolute;
+}
+
+
+.product-details{
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    z-index: 1;
+    height: 70vh;
+    width: 50vh;
+    position: relative;
+}
+
+
+.overlay-product{
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    background-color: black;
+    z-index: 1;
+    height: 70vh;
+    width: 50vh;
+    position: absolute;
+    opacity: 0.5;
+    border-radius: var(--radius);
+}
+
+.product-details:hover{
+  backdrop-filter: blur(5px);
+  }
+
+
+.item-info header {
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 0.5px dotted var(--clr-grey-5);
+}
+
+.item-info h4 {
+  margin-bottom: 0.5rem;
+}
+
+.price {
+  position: relative;
+  text-align: center;
+  color: var(--clr-gold);
+}
+
+.description-holder {
+  position: relative;
+  display: inline;
+  padding: 1rem;
+  color: white;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+
+.product-header{
+    width: 100%;
+    display: grid;
+    position: relative;
+    align-items: center;
+    justify-content: center;
+}
+
+.item-text {
+  color: white;
+}
+
+.cardsHolder{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  place-content: center;
+}
+
+ @media only screen and (max-width: 600px) {
+  .cardsHolder{
+    display: grid;
+   }
  }
 `
 
