5982693335aa24b9c9b8b8baa722c43ac8a698a1
PJ <87795990+pauldidonatojr@users.noreply.github.com>
2023-02-22 16:19:16 -0500

    push me

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -11,15 +11,21 @@ import { FaWindowClose } from 'react-icons/fa'
 import Contact from '../components/Contact.js'
 import RainbowChat from '../components/RainbowChat.js'
 
+import Modal from 'react-bootstrap/Modal'
+import Form from 'react-bootstrap/Form'
 const Landing = () => {
  const [data, setData] = useState(heroData)
  const [index, setIndex] = useState(0)
+ const [show, setShow] = useState(false)
+ const [currentImage, setCurrentImage] = useState(0)
  const { openModal } = useModalContext()
  const { openChat } = useModalContext()
- const [currentImage, setCurrentImage] = useState(0)
  const { isModalOpen, closeModal } = useModalContext()
  const { isChatOpen, closeChat } = useModalContext()
+ const handleClose = () => setShow(false)
+ const handleShow = () => setShow(true)
  // const [showChat, setShowChat] = useState(false)
+
  const backgroundImages = [
   'https://res.cloudinary.com/elpawl-llc/image/upload/v1675866967/skyline_fhftgi.jpg',
   'https://res.cloudinary.com/elpawl-llc/image/upload/v1676508521/pexels-filippo-bergamaschi-2767737_fidye2.jpg',
@@ -41,6 +47,8 @@ const Landing = () => {
    setIndex(0)
   }
  }, [index, data])
+
+ /////////////////////////
  useEffect(() => {
   const intervalId = setInterval(() => {
    setCurrentImage((currentImage + 1) % backgroundImages.length)
@@ -48,6 +56,7 @@ const Landing = () => {
 
   return () => clearInterval(intervalId)
  }, [currentImage, backgroundImages.length])
+ /////////////////////////
 
  useEffect(() => {
   let slider = setInterval(() => {
@@ -57,6 +66,53 @@ const Landing = () => {
    clearInterval(slider)
   }
  }, [index])
+
+ const card = (
+  <ContactCard>
+   <Modal
+    size="lg"
+    aria-labelledby="contained-modal-title-vcenter"
+    centered
+    show={show}
+    onHide={handleClose}
+   >
+    <Modal.Header closeButton>
+     <Modal.Title>Contact Us</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+     <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+       <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+       <Form.Control type="text" placeholder="Name" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+       <Form.Control type="phone" placeholder="Phone" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+       <Form.Control
+        as="textarea"
+        rows={3}
+        placeholder="Type your message here"
+       />
+      </Form.Group>
+     </Form>
+    </Modal.Body>
+    <Modal.Footer>
+     <Button variant="secondary" onClick={handleClose}>
+      Close
+     </Button>
+     <Button variant="primary" onClick={handleClose}>
+      Submit
+     </Button>
+    </Modal.Footer>
+   </Modal>
+  </ContactCard>
+ )
  //  function Popup() {
  //   const [showPopup, setShowPopup] = useState(true)
 
@@ -86,10 +142,10 @@ const Landing = () => {
    {/* <div className="app">
     <Popup />
    </div> */}
-
+   <>{card}</>
    <div className="container page ">
     {/* <div className={`${setShowChat ? 'info' : 'info-overlay'}`}> */}
-    <div className="info">
+    <div className="info" style={{border: '5px solid black'}}>
      {/* <div className="navbar"> Information </div> */}
 
      <h1 style={{ fontSize: '3rem' }}>
@@ -136,7 +192,11 @@ const Landing = () => {
          enter
         </Link>
        </Button>
-       <Button variant="contained" onClick={openModal} className="btn btn-hero">
+       <Button
+        variant="contained"
+        onClick={handleShow}
+        className="btn btn-hero"
+       >
         contact us
        </Button>
        <Button variant="contained" onClick={openChat} className="btn btn-hero">
@@ -155,7 +215,7 @@ const Landing = () => {
           style={{ position: 'relative', zIndex: '1', top: '0', right: '0' }}
          ></FaWindowClose>
         </button>
-        <Contact />
+        Contact
        </div>
       </div>
 
@@ -612,7 +672,240 @@ const Wrapper = styled.main`
   }
  }
 `
+const ContactCard = styled.div`
+ height: 100%;
+ width: 100%;
+ .background {
+  display: flex;
+  min-height: 100vh;
+ }
 
+ .container {
+  flex: 0 1 700px;
+  margin: auto;
+  padding: 10px;
+ }
+
+ .screen {
+  position: relative;
+  background: #3e3e3e;
+  border-radius: 15px;
+ }
+
+ .screen:after {
+  content: '';
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 20px;
+  right: 20px;
+  bottom: 0;
+  border-radius: 15px;
+  box-shadow: 0 20px 40px rgba(0, 0, 0, 0.4);
+  z-index: -1;
+ }
+
+ .screen-header {
+  display: flex;
+  align-items: center;
+  padding: 10px 20px;
+  background: #4d4d4f;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+ }
+
+ .screen-header-left {
+  margin-right: auto;
+ }
+
+ .screen-header-button {
+  display: inline-block;
+  width: 8px;
+  height: 8px;
+  margin-right: 3px;
+  border-radius: 8px;
+  background: white;
+ }
+
+ .screen-header-buttonClose {
+  display: inline-block;
+  width: 8px;
+  height: 8px;
+  margin-right: 3px;
+  border-radius: 8px;
+ }
+
+ .screen-header-button.close {
+ }
+
+ .screen-header-button.maximize {
+  background: #e8e925;
+ }
+
+ .screen-header-button.minimize {
+  background: #74c54f;
+ }
+
+ .screen-header-right {
+  display: flex;
+ }
+
+ .screen-header-ellipsis {
+  width: 3px;
+  height: 3px;
+  margin-left: 2px;
+  border-radius: 8px;
+  background: #999;
+ }
+
+ .screen-body {
+  display: flex;
+ }
+
+ .screen-body-item {
+  flex: 1;
+  padding: 50px;
+ }
+
+ .screen-body-item.left {
+  display: flex;
+  flex-direction: column;
+ }
+
+ .app-title {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  color: #ea1d6f;
+  font-size: 26px;
+ }
+
+ .app-title:after {
+  content: '';
+  display: block;
+  position: absolute;
+  left: 0;
+  bottom: -10px;
+  width: 25px;
+  height: 4px;
+  background: #ea1d6f;
+ }
+
+ .app-contact {
+  margin-top: auto;
+  font-size: 8px;
+  color: #888;
+ }
+
+ .app-form-group {
+  margin-bottom: 15px;
+ }
+
+ .app-form-group.message {
+  margin-top: 40px;
+ }
+
+ .app-form-group.buttons {
+  margin-bottom: 0;
+  text-align: right;
+ }
+
+ .app-form-control {
+  width: 100%;
+  padding: 10px 0;
+  background: none;
+  border: none;
+  border-bottom: 1px solid #666;
+  color: #ddd;
+  font-size: 14px;
+  text-transform: uppercase;
+  outline: none;
+  transition: border-color 0.2s;
+ }
+
+ .app-form-control::placeholder {
+  color: #666;
+ }
+
+ .app-form-control:focus {
+  border-bottom-color: #ddd;
+ }
+
+ .app-form-button {
+  background: none;
+  border: none;
+  color: white;
+  font-size: 14px;
+  cursor: pointer;
+  outline: none;
+ }
+
+ .app-form-button:hover {
+  color: brown;
+  font-weight: bold;
+ }
+
+ .credits {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+  color: #ffa4bd;
+  font-family: 'Roboto Condensed', sans-serif;
+  font-size: 16px;
+  font-weight: normal;
+ }
+
+ .credits-link {
+  display: flex;
+  align-items: center;
+  color: #fff;
+  font-weight: bold;
+  text-decoration: none;
+ }
+
+ .dribbble {
+  width: 20px;
+  height: 20px;
+  margin: 0 5px;
+ }
+
+ @media screen and (max-width: 520px) {
+  .screen-body {
+   flex-direction: column;
+  }
+
+  .screen-body-item.left {
+   margin-bottom: 30px;
+  }
+
+  .app-title {
+   flex-direction: row;
+  }
+
+  .app-title span {
+   margin-right: 12px;
+  }
+
+  .app-title:after {
+   display: none;
+  }
+ }
+
+ @media only screen and (max-width: 600px) {
+  .app-contact {
+   display: none;
+  }
+
+  .screen-body {
+   padding: 40px;
+  }
+
+  .screen-body-item {
+   padding: 0;
+  }
+ }
+`
 //  @media (min-width: 576px) {
 //   .page {
 //    /* min-height: calc(100vh - var(--nav-height)); */
